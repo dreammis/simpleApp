8a3e57d9d0db58325ac8c0d145e07b71af499c91
dreammis <hysn2001@gmail.com>
2015-11-11 23:45:41 +0800

    这个看来要写不完了，我要进行下一项训练了，面向对象路漫漫

--- saleHouse.py
+++ saleHouse.py
@@ -15,7 +15,7 @@ class Property():
         print "bathrooms:{}".format(self.num_baths)
         print()
 
-    def prompt_init():
+    def prompt_init(self):
         return dict(square_feet = raw_input("Enter the square feet:"),
                     beds = raw_input("Enter the number of bedrooms:"),
                     baths = raw_input("Enther the number of bathrooms:"))
@@ -38,17 +38,16 @@ class Apartment(Property):
         print "laundry:%s"%self.laundry
         print "has balcony:%s"%self.balcony
 
+    def prompt_init(self):
+
         parent_init=Property.prompt_init()
-        laundry = ''
-        while laundry.lower() not in Apartment.valid_laundries:
-            laundry = raw_input("What laundry facilities does"
-                                "the property have? ({})").format(",".join(Apartment.valid_laundries))
-        balcony = ''
-        while balcony.lower() not in Apartment.valid_balconies:
-            balcony = raw_input(
-                "Does the property have a balcony?"
-                "({})".format(",".join(Apartment.valid_balconies))
-            )
+        laundry = get_valid_input(""
+                                  "What laundry facilities does"
+                                  "the property have?",
+                                  Apartment.valid_laundries)
+        balcony = get_valid_input(""
+                                  "Does the property have a balcony?",
+                                  Apartment.valid_balconies)
         parent_init.update(
             {
                 "laundry":laundry,
@@ -56,7 +55,41 @@ class Apartment(Property):
             }
         )
         return parent_init
-        prompt_init = staticmethod(prompt_init)
+    prompt_init = staticmethod(prompt_init)
+
+class House(Property):
+    valid_garage = ("attached","detached","none")
+    valid_fenced = ("yes","no")
+
+    def __init__(self,num_stories='',
+                 garage='',fenced='',**kwargs):
+        super.__init__(**kwargs)
+        self.garage = garage
+        self.fenced = fenced
+        self.num_stories = num_stories
+
+    def display(self):
+        super.display()
+        print "HOUSE DETAILS"
+        print "# of stories:{}".format(self.num_stories)
+        print "garage:{}".format(self.garage)
+        print "fenced yard:{}".format(self.fenced)
+
+    def prompt_init(self):
+        parent_init = Property.prompt_init()
+        fenced = get_valid_input("Is the yard fenced?",House.valid_fenced)
+        garage = get_valid_input("Is there a garage?",House.valid_garage)
+        num_stories = raw_input("How many stories?")
+        parent_init.update({
+            "fenced":fenced,
+            "garage":garage,
+            "num_stories":num_stories
+        })
+        return parent_init
+    prompt_init = staticmethod(prompt_init)
+
+
+
 
 def get_valid_input(input_string,valid_options):
     input_string += "({})".format(",".join(valid_options))
